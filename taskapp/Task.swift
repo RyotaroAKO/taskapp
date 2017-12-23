67e64f78f0854149c12f453dca18d0e1bacdad65
ryoutarouakou <allergy.blue@gmail.com>
2017-12-24 08:47:33 +0900

    タスク管理アプリ（Lesson7.5まで）

---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 赤穂亮太郎 on 2017/12/22.
+//  Copyright © 2017年 ryoutarou.akou. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
